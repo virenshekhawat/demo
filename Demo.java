fb6e602c50f7a766874e93d55207931a3dad4d7f
Virendra Shekhawat <virenshekhawat25@gmail.com>
2016-08-24 13:02:45 +0530

    Update Demo.java
    Demo .java updated

--- Demo.java
+++ Demo.java
@@ -1,5 +1,6 @@
 Class Demo {
   public static void main (string[] args){
     System.out.println("This is sample dummi program");
+    System.out.println("This code was modified on aug,24 2016 @ 1:00pm");
   }
 }
